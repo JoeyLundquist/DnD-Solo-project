1a8bbf10a66ecbd04edd24918ef721caef2dee60
Joey Lundquist <joeylundquist91@gmail.com>
2022-07-12 21:22:37 -0500

    Finished the base functions for money management

--- src/components/MoniesManager/MoniesManager.jsx
+++ src/components/MoniesManager/MoniesManager.jsx
@@ -10,6 +10,10 @@ const MoniesManager = () => {
 
     const [editMode, setEditMode] = useState(false)
     const [copper, setCopper] = useState(monies.copper_pieces)
+    const [silver, setSilver] = useState(monies.silver_pieces)
+    const [electrum, setElectrum] = useState(monies.electrum_pieces)
+    const [gold, setGold] = useState(monies.gold_pieces)
+    const [platinum, setPlatinum] = useState(monies.platinum_pieces)
     
 
 
@@ -18,13 +22,14 @@ const MoniesManager = () => {
             type: 'UPDATE_MONIES',
             payload: {
                 charId: monies.id,
-                copper,
+                copper: monies.copper_pieces,
                 silver: monies.silver_pieces,
                 electrum: monies.electrum_pieces,
-                // gold: monies.silver_pieces,
-                platinum: monies.platinum_pieces,
+                gold: monies.gold_pieces,
+                platinum: monies.platinum_pieces
             }
         })
+        setEditMode(!editMode)
     }
 
 
@@ -36,7 +41,13 @@ const MoniesManager = () => {
                     {
                         editMode ? 
                         <>
-                            <input type="text" placeholder="..." className="monies-update-input" value={copper} onChange={e => setCopper(e.target.value)} onBlur={() => setEditMode(!editMode)} />
+                            <input
+                                type="text" 
+                                placeholder="..." 
+                                className="monies-update-input" 
+                                value={monies.copper_pieces} 
+                                onChange={(e) => dispatch({type: 'UPDATE_COPPER_PIECES', payload: e.target.value})}
+                            />
                             <button onClick={updateMonies}>Submit</button>
                         </>
                         :
@@ -46,16 +57,75 @@ const MoniesManager = () => {
 
                 </div>
                 <div className="monies-count">
-                    Silver: <br></br>{monies.silver_pieces}
+                    Silver: <br></br>
+                    {
+                        editMode ? 
+                        <>
+                            <input 
+                                type="text" 
+                                placeholder="..." 
+                                className="monies-update-input" 
+                                value={monies.silver_pieces} 
+                                onChange={(e) => dispatch({type: 'UPDATE_SILVER_PIECES', payload: e.target.value})} 
+                            />
+                            <button onClick={updateMonies}>Submit</button>
+                        </>
+                        :
+                        <p onClick={() => setEditMode(!editMode)}>{monies.silver_pieces} </p>
+                     
+                    }
                 </div>
                 <div className="monies-count">
-                    Electrum: <br></br>{monies.electrum_pieces}
+                    Electrum: <br></br>
+                    {
+                        editMode ? 
+                        <>
+                            <input type="text" 
+                            placeholder="..." 
+                            className="monies-update-input" 
+                            value={monies.electrum_pieces} 
+                            onChange={(e) => dispatch({type: 'UPDATE_ELECTRUM_PIECES', payload: e.target.value})}/>
+                            
+                            <button onClick={updateMonies}>Submit</button>
+                        </>
+                        :
+                        <p onClick={() => setEditMode(!editMode)}>{monies.electrum_pieces} </p>
+                     
+                    }
                 </div>
                 <div className="monies-count">
-                    Gold: <br></br>5
+                    Gold: <br></br>
+                    {
+                        editMode ? 
+                        <>
+                            <input 
+                            type="text" placeholder="..." 
+                            className="monies-update-input" 
+                            value={monies.gold_pieces} 
+                            onChange={(e) => dispatch({type: 'UPDATE_GOLD_PIECES', payload: e.target.value})}/>
+                            <button onClick={updateMonies}>Submit</button>
+                        </>
+                        :
+                        <p onClick={() => setEditMode(!editMode)}>{monies.gold_pieces} </p>
+                     
+                    }
                 </div>
                 <div className="monies-count">
-                    Platinum: <br></br>{monies.platinum_pieces}
+                    Platinum: <br></br>
+                    {
+                        editMode ? 
+                        <>
+                            <input type="text" 
+                            placeholder="..." 
+                            className="monies-update-input" 
+                            value={monies.platinum_pieces} 
+                            onChange={(e) => dispatch({type: 'UPDATE_PLATINUM_PIECES', payload: e.target.value})}/>
+                            <button onClick={updateMonies}>Submit</button>
+                        </>
+                        :
+                        <p onClick={() => setEditMode(!editMode)}>{monies.platinum_pieces} </p>
+                     
+                    }
                 </div>
             </div>
         </>
--- src/redux/reducers/character.reducer.js
+++ src/redux/reducers/character.reducer.js
@@ -4,6 +4,16 @@ export const characterReducer = (state = {}, action) => {
             return action.payload;
         case 'SET_CHARACTER_INFO':
             return action.payload
+        case 'UPDATE_COPPER_PIECES':
+            return {...state, copper_pieces: action.payload}
+        case 'UPDATE_SILVER_PIECES':
+            return {...state, silver_pieces: action.payload}
+        case 'UPDATE_ELECTRUM_PIECES':
+            return {...state, electrum_pieces: action.payload}
+        case 'UPDATE_GOLD_PIECES':
+            return {...state, gold_pieces: action.payload}
+        case 'UPDATE_PLATINUM_PIECES':
+            return {...state, platinum_pieces: action.payload}
 
         default:
             return state;
